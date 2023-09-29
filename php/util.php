7a372e821bae0be602f3c202f39445fc186862df
Coelhozo <pedrowwww44@gmail.com>
2023-09-29 16:48:36 -0300

    cabei por hj

--- php/util.php
+++ php/util.php
@@ -1,9 +1,15 @@
 <?php
     include 'conectorBD.php';
 
-    function converterChave($chave, $tabela, $modo=1){
-        if ($modo){
-
+    function converterChave($chave, $tabela, $modo=1)
+    {
+        if ($modo)
+        {
+            $valorChave = executarQuery("SELECT * FROM $tabela WHERE id == $chave", $retorno=1);
+        }
+        else
+        {
+            $idChave = executarQuery("SELECT id from $tabela WHERE id == $chave;", $retorno=1);
         }
     }
 ?>
