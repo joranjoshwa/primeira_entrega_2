547cdb34ba8d0ce472859f2e20669339c22bdf2c
Coelhozo <pedrowwww44@gmail.com>
2023-10-03 15:14:28 -0300

    atualização dos inserts

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -1,3 +1,5 @@
 INSERT INTO `localidades` (`id`, `nome`) VALUES (NULL, 'Eunápolis');
+INSERT INTO `localidades` (`id`, `nome`) VALUES (NULL, 'Salto da Divisa');
+INSERT INTO `localidades` (`id`, `nome`) VALUES (NULL, 'Porto Seguro');
 INSERT INTO `instituicoes` (`id`, `nome`, `CNPJ`, `email`, `senha`, `localidades_id`) VALUES (NULL, 'IFBA', '23423434', 'pedro2323@gmail.com', 'wewewe', '1');
 INSERT INTO `agricultores` (`id`, `nome`, `CAF`, `CPF`, `senha`, `telefone`, `email`, `localidades_id`) VALUES (NULL, 'pero', '23213321232', '31123123', '123123123', '123123123', '213213123', '1');
